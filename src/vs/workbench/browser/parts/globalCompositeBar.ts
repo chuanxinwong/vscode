8aa5e293b12cd09aa03443f8e85b904f2ce58770
chunxinwang <gengzhii@hotmail.com>
2026-07-03 11:12:52 +0800

    默认关闭账户;

--- src/vs/workbench/browser/parts/globalCompositeBar.ts
+++ src/vs/workbench/browser/parts/globalCompositeBar.ts
@@ -742,7 +742,7 @@ function simpleActivityContextMenuActions(storageService: IStorageService, isAcc
 }
 
 export function isAccountsActionVisible(storageService: IStorageService): boolean {
-	return storageService.getBoolean(AccountsActivityActionViewItem.ACCOUNTS_VISIBILITY_PREFERENCE_KEY, StorageScope.PROFILE, true);
+	return storageService.getBoolean(AccountsActivityActionViewItem.ACCOUNTS_VISIBILITY_PREFERENCE_KEY, StorageScope.PROFILE, !true);
 }
 
 function setAccountsActionVisible(storageService: IStorageService, visible: boolean) {
